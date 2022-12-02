beaaaa84b897ffcf09e5cf8a84dfbb9a67a7278c
Keivy Mora <mkeivy.mora@gmail.com>
2022-12-02 19:42:09 -0400

    add time WS

--- src/prediction/prediction.service.ts
+++ src/prediction/prediction.service.ts
@@ -91,10 +91,17 @@ export class PredictionService {
     this.updatePoints();
   }
 
-  @Cron('25 19 * * *') //09:30pm
+  @Cron('44 19 * * *') //09:30pm
   async testLocal() {
-    console.log('Actualizando a las 7:25PM hora local');
-    this.logger.log('Actualizando a las  7:25PM hora local');
+    console.log('Actualizando a las 7:44PM hora local');
+    this.logger.log('Actualizando a las  7:44PM hora local');
+    this.updatePoints();
+  }
+
+  @Cron('44 18 * * *') //09:30pm
+  async testLocalWS() {
+    console.log('Actualizando a las 7:44PM hora local coparada con WS');
+    this.logger.log('Actualizando a las  7:44PM hora local coparada con WS');
     this.updatePoints();
   }
 
