6d2e9334ecdb25c2605cd3776de34b497045b0d6
Keivy Mora <mkeivy.mora@gmail.com>
2022-12-02 17:13:43 -0400

    delete engines

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
 
   app.enableCors({
     origin: configService.get('CLIENT_URL'),
-    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
   });
 
   app.useGlobalPipes(
